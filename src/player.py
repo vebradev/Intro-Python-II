b16295a85bb77a7ef6883e063c1501548a7466e1
vebradev <justinas@vebra.dev>
2020-02-13 15:07:03 +0000

    add methods to get & drop items in Player class

--- src/player.py
+++ src/player.py
@@ -9,9 +9,9 @@ class Player:
         self.items = items
 
     def __str__(self):
-        output = f"{self.name}, you entered {self.location}."
+        output = f"{self.name}, you are located at the {self.location}"
         if self.items:
-            output += " Items:"
+            output += " Items:" if len(self.items) > 1 else " Item:"
             for i in self.items:
                 if self.items[-1] == i:
                     output += f" {i}"
@@ -21,8 +21,28 @@ class Player:
             return output
         return output
 
+    def get(self, item):
+        if self.items == None:
+            self.items = []
+        self.items.append(item)
+        print(f"{item} has been added to your inventory.")
+
+    def drop(self, item):
+        if self.items:
+            if self.items.__contains__(item):
+                target = self.items.index(item)
+                self.items.pop(target)
+                print(f"{item} has been removed from your inventory.")
+            else:
+                print("No such item in your inventory.")
+        else:
+            print("Your inventory is empty.")
+
 
 # p1 = Player("Michael", "Loc 1")
+# p1.get("Axe")
+# p1.get("Showel")
+# print(p1)
 # p2 = Player("Michael", "Loc 2", ["Item 1", "Item 2", "Item 3"])
 # p3 = Player("Michael", "Loc 3", ["Item 1"])
 
