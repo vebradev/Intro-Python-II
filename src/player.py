db9547eb6126fe38128fc4d5bfc9dd60d20352f2
vebradev <justinas@vebra.dev>
2020-02-13 13:08:50 +0000

    extend Player class to have items

--- src/player.py
+++ src/player.py
@@ -1,10 +1,31 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
+
 class Player:
-    def __init__(self, name, location):
+    def __init__(self, name, location, items=None):
         self.name = name
         self.location = location
-    
+        self.items = items
+
     def __str__(self):
-        return f"{self.name}, you entered {self.location}."
+        output = f"{self.name}, you entered {self.location}."
+        if self.items:
+            output += " Items:"
+            for i in self.items:
+                if self.items[-1] == i:
+                    output += f" {i}"
+                else:
+                    output += f" {i},"
+            output += "."
+            return output
+        return output
+
+
+# p1 = Player("Michael", "Loc 1")
+# p2 = Player("Michael", "Loc 2", ["Item 1", "Item 2", "Item 3"])
+# p3 = Player("Michael", "Loc 3", ["Item 1"])
+
+# print(p1)
+# print(p2)
+# print(p3)
