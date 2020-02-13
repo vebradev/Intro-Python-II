921029c9b6e6fe97a76b7880ea850eace7100e9c
vebradev <justinas@vebra.dev>
2020-02-13 21:59:10 +0000

    implement list_items, get & drop methods in Room class

--- src/room.py
+++ src/room.py
@@ -1,6 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+from item import Item
+
 
 class Room:
     def __init__(self, name, description, items=None):
@@ -9,24 +11,51 @@ class Room:
         self.items = items
 
     def __str__(self):
-        output = f"Room: {self.name}. Description: {self.description}."
+        output = f"{self.name}. {self.description}"
         if self.items:
-            output += " Items:"
-            for i in self.items:
-                if self.items[-1] == i:
-                    output += f" {i}"
-                else:
-                    output += f" {i},"
-            output += "."
+            output += " " + self.list_items()
             return output
         else:
             return output
 
+    def list_items(self):
+        if self.items:
+            output = "Items in the room:" if len(
+                self.items) > 1 else "One item in the room:"
+            for i in self.items:
+                if self.items[-1] == i:
+                    output += f" {i.name}"
+                else:
+                    output += f" {i.name},"
+            output += "."
+            return output
+        else:
+            "There are no items in this room."
 
-# a = Room("A ROOM", "Not so big actually", ["item", "item2"])
-# b = Room("B ROOM", "Smaller than A", ["item"])
+    def get(self, item):
+        if self.items == None:
+            self.items = []
+        self.items.append(item)
+        print(f"{item.name} was dropped in {self.name}.")
+
+    def drop(self, item):
+        if self.items:
+            for i, v in enumerate(self.items):
+                if v.name == item:
+                    self.items.pop(i)
+                    return v
+        else:
+            print(f"No items found in {self.name}")
+
+
+# a = Room("A ROOM", "Not so big actually", [
+#          Item("Axe", "rusty"), Item("Showel", "goldigger")])
+# b = Room("B ROOM", "Smaller than A", [Item("Cheese", "smelly")])
 # c = Room("C ROOM", "Smallest of em all")
 
 # print(a)
 # print(b)
 # print(c)
+
+# apple = Item("Apple", "sweeeet")
+# cup = Item("Cup", "broken as hell")
