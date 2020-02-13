36d6bd1380d1e417f42f85e006da926d2df1a9f6
vebradev <justinas@vebra.dev>
2020-02-13 21:57:36 +0000

    refactor command parser in adv.py

--- src/adv.py
+++ src/adv.py
@@ -1,11 +1,12 @@
 from room import Room
+from item import Item
 from player import Player
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons.", [Item("stick", "wet, short and not very useful"), Item("rock", "fits perfectly in your palm")]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -15,7 +16,7 @@ into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", [Item("showel", "rusty, but still does the job")]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
@@ -40,23 +41,18 @@ room['treasure'].s_to = room['narrow']
 
 messages = {
     'welcome': "\nWelcome to THE ADVENTURE GAME\n",
-    'name_input': "What is your name, young grasshopper?\n",
-    'dir_input': "\nWhich direction do you want to move?\nN, E, S, W: ",
+    'name_input': "What is your name, young grasshopper?\nName: ",
     'warning_dir': "No such direction from this room."
 }
+# location = room['outside']
 
 print(messages["welcome"])
-
 name = input(messages["name_input"])
-
-location = room['outside']
-
-status = True
-
 
 # Make a new player object that is currently in the 'outside' room.
 
-player = Player(name, location)
+player = Player(name, room['outside'])
+status = True if player.name else False
 
 # Write a loop that:
 #
@@ -69,44 +65,73 @@ player = Player(name, location)
 #
 # If the user enters "q", quit the game.
 
+commands = ["n", "e", "s", "w", "q", "get", "drop"]
+
 while status:
-    print(f"\n{player}")
-
-    direction = input(messages["dir_input"]).upper()
-
-    if len(direction) >= 1:
-        if direction[0] == "N":
-            if hasattr(player.location, "n_to"):
-                print(f"{player.name} decided to move North.")
-                player.location = location.n_to
-                location = location.n_to
-            else:
-                print(messages["warning_dir"])
-        elif direction[0] == "E":
-            if hasattr(player.location, "e_to"):
-                print(f"{player.name} decided to move East.")
-                player.location = location.e_to
-                location = location.e_to
-            else:
-                print(messages["warning_dir"])
-        elif direction[0] == "S":
-            if hasattr(player.location, "s_to"):
-                print(f"{player.name} decided to move South.")
-                player.location = location.s_to
-                location = location.s_to
-            else:
-                print(messages["warning_dir"])
-        elif direction[0] == "W":
-            if hasattr(player.location, "w_to"):
-                print(f"{player.name} decided to move West.")
-                player.location = location.w_to
-                location = location.w_to
-            else:
-                print(messages["warning_dir"])
-        elif direction[0] == "Q":
-            print(f"Thanks for playing {player.name}. Bye for now!")
+    print("\n======================================================\n")
+    print(f"{player}")
+    user_input = input("\nTo change direction, enter: \"n\", \"e\", \"s\", \"w\". \nTo perform action, enter: \"take [item]\" or \"drop [item]\"\nCommand: ")
+    
+    if len(user_input) == 1:
+        direction = user_input[0]
+        if direction == "q":
+            print("Thanks for playing.")
             status = False
+        elif direction in commands[:4]:
+            dir_attr = direction + "_to"
+            if hasattr(player.location, dir_attr):
+                player.location = getattr(player.location, dir_attr)
+            else:
+                print("Direction does not exist. Look around.")
         else:
-            print("No such direction.")
-    else:
-        status = False
+            print("Command not found.")
+    elif len(user_input) >= 2:
+        command = user_input.split()[0]
+        print(command)
+        pass
+    # status = False
+
+# while status:
+# print(f"\n{player}")
+
+# action = input("\nWhich direction do you want to move?\nN, E, S, W: ").lower()
+
+# if len(action) == 1:
+#     if action[0] == "n":
+#         if hasattr(player.location, "n_to"):
+#             print(f"{player.name} decided to move North.")
+#             player.location = location.n_to
+#             location = location.n_to
+#         else:
+#             print(messages["warning_dir"])
+#     elif action[0] == "e":
+#         if hasattr(player.location, "e_to"):
+#             print(f"{player.name} decided to move East.")
+#             player.location = location.e_to
+#             location = location.e_to
+#         else:
+#             print(messages["warning_dir"])
+#     elif action[0] == "s":
+#         if hasattr(player.location, "s_to"):
+#             print(f"{player.name} decided to move South.")
+#             player.location = location.s_to
+#             location = location.s_to
+#         else:
+#             print(messages["warning_dir"])
+#     elif action[0] == "w":
+#         if hasattr(player.location, "w_to"):
+#             print(f"{player.name} decided to move West.")
+#             player.location = location.w_to
+#             location = location.w_to
+#         else:
+#             print(messages["warning_dir"])
+#     elif action[0] == "q":
+#         print(f"Thanks for playing {player.name}. Bye for now!")
+#         status = False
+#     else:
+#         print("No such direction.")
+# elif len(action) >= 2:
+#     commands = action.split()
+#     print(f"{commands}")
+# else:
+#     status = False
